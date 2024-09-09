637ca0877ebdc1f8088af1a7f70c441b9166faea
hhamnnes <96078953+hhamnnes@users.noreply.github.com>
2024-09-09 19:56:18 +0200

    I have changed the dependecies

--- bettermemory/src/main/java/no/bettermemory/models/time/Day.java
+++ bettermemory/src/main/java/no/bettermemory/models/time/Day.java
@@ -1,5 +1,9 @@
 package no.bettermemory.models.time;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+
 public class Day {
     
 }
