8727a120acd479995e5c18b43cfd5f68ec1d4606
hhamnnes <96078953+hhamnnes@users.noreply.github.com>
2024-09-11 19:36:28 +0200

    Added documentation for day class

--- bettermemory/src/main/java/no/bettermemory/models/time/Day.java
+++ bettermemory/src/main/java/no/bettermemory/models/time/Day.java
@@ -5,7 +5,19 @@ import java.util.ArrayList;
 import no.bettermemory.models.activity.Activity;
 
 
-
+/**
+ * This class is ment to represent a day-object. This object will contain a list of 
+ * activities ment for the pasient. This object will have a key role in week-class objects.
+ * 
+ * @param dayName - Is one of the following: monday, tuesday, wednesday, thursday, friday, saturday or sunday.
+ * @param activities - Is a list of object of the class Activity.
+ * 
+ * @author Hermann Mjelde Hamnnes
+ * 
+ * @code
+ * This is how you can create an object of this class:
+ * <pre>{@code Day day = new Day("Monday", activities);} </pre>
+ */
 public class Day {
 
     private String dayName;
