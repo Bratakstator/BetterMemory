aac61fc2460f8fa3a954993bd66cd99244c83b37
JoakimKL <88777720+Bratakstator@users.noreply.github.com>
2024-09-14 14:55:52 +0200

    Set up ToMongoDB

--- bettermemory/src/main/java/no/bettermemory/models/storageHandlers/ToMongoDB.java
+++ bettermemory/src/main/java/no/bettermemory/models/storageHandlers/ToMongoDB.java
@@ -1,6 +1,7 @@
 package no.bettermemory.models.storageHandlers;
 
 import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
@@ -10,4 +11,33 @@ public class ToMongoDB implements ToDatabase {
     private MongoClient client;
     private MongoDatabase database;
     private MongoCollection collection;
+
+    public ToMongoDB(String connectionString, String databaseName, String collectionName) {
+        this.client = MongoClients.create(connectionString);
+        this.database = this.client.getDatabase(databaseName);
+        this.collection = this.database.getCollection(collectionName);
+    }
+
+    public void setClient(String connectionString) {
+        this.client = MongoClients.create(connectionString);
+    }
+
+    public void setDatabase(String databaseName) {
+        this.database = this.client.getDatabase(databaseName);
+    }
+
+    public void setCollection(String collectionName) {
+        this.collection = this.database.getCollection(collectionName);
+    }
+
+    public MongoClient getClient() {
+        return client;
+    }
+    public MongoDatabase getDatabase() {
+        return database;
+    }
+
+    public MongoCollection getCollection() {
+        return collection;
+    }
 }
