60543bad164595f086aa51abe40049fd693a17f9
hhamnnes <96078953+hhamnnes@users.noreply.github.com>
2024-09-26 16:34:55 +0200

    I have changed the ObjectToDatabse-interface

--- bettermemory/src/main/java/no/bettermemory/interfaces/storageHandlers/ObjectToDatabase.java
+++ bettermemory/src/main/java/no/bettermemory/interfaces/storageHandlers/ObjectToDatabase.java
@@ -1,15 +1,8 @@
 package no.bettermemory.interfaces.storageHandlers;
 
+
 public interface ObjectToDatabase<T> {
 
-    /*
-     * This methode should check if a given object already exists in the database.
-     */
-    boolean alreadyExists(T object) throws IllegalArgumentException;
-
-    /*
-     * This methode should write a given object to a given database.
-     */
-    void save(T object);
+    public void save(T ObjectToDatabase);
 
 }
