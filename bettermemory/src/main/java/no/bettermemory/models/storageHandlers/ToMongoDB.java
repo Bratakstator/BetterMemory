a9f12ca0404935f3742957ce41f8379184e4a4cc
JoakimKL <88777720+Bratakstator@users.noreply.github.com>
2024-09-29 21:17:45 +0200

    First iteration of saveObject() complete

--- bettermemory/src/main/java/no/bettermemory/models/storageHandlers/ToMongoDB.java
+++ bettermemory/src/main/java/no/bettermemory/models/storageHandlers/ToMongoDB.java
@@ -1,12 +1,23 @@
 package no.bettermemory.models.storageHandlers;
 
-import org.bson.Document;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import com.mongodb.MongoBulkWriteException;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.InsertManyResult;
+import com.mongodb.client.result.InsertOneResult;
 
 import no.bettermemory.interfaces.storageHandlers.ToDatabase;
+import no.bettermemory.models.activity.Activity;
+import no.bettermemory.models.time.Day;
+import no.bettermemory.models.time.Week;
 import no.bettermemory.models.users.HealthCarePersonnel;
 import no.bettermemory.models.users.Patient;
 import no.bettermemory.tools.DatabaseConnections;
@@ -43,10 +54,38 @@ public class ToMongoDB implements ToDatabase {
             Document document = ((Patient) object).toDocument();
             collection.insertOne(document);
         }
-        if (object instanceof HealthCarePersonnel) {
+        else if (object instanceof HealthCarePersonnel) {
             MongoCollection<Document> collection = DatabaseConnections.getHealthCarePersonnelCollection(database);
             Document document = ((HealthCarePersonnel) object).toDocument();
             collection.insertOne(document);
+        }
+        else if (object instanceof Week) {
+            MongoCollection<Document> activityCollection = DatabaseConnections.getActivitiesCollection(database);
+            MongoCollection<Document> dayCollection = DatabaseConnections.getDaysCollection(database);
+            MongoCollection<Document> weekCollection = DatabaseConnections.getWeeksCollection(database);
+
+            Week week = (Week) object;
+            ArrayList<Day> days = week.getDays();
+            List<ObjectId> dayResultValues = new ArrayList<>();
+            for (Day day : days) {
+                List<Document> activities = day.getActivities().stream().map(activity -> activity.toDocument()).collect(Collectors.toList());
+                List<ObjectId> insertResultValues;
+                try {
+                    InsertManyResult insertResult = activityCollection.insertMany(activities);
+                    insertResultValues = insertResult.getInsertedIds().values()
+                    .stream().map(document -> document.asObjectId().getValue()).collect(Collectors.toList());
+                } catch (MongoBulkWriteException e) {
+                    insertResultValues = e.getWriteResult().getInserts().stream()
+                    .map(document -> document.getId().asObjectId().getValue()).collect(Collectors.toList());
+                }
+                Document dayDocument = day.toDocument();
+                dayDocument.append("activities", insertResultValues);
+                InsertOneResult insertOneResult = dayCollection.insertOne(dayDocument);
+                dayResultValues.add(insertOneResult.getInsertedId().asObjectId().getValue());
+            }
+            Document weekDocument = week.toDocument();
+            weekDocument.append("days", dayResultValues);
+            weekCollection.insertOne(weekDocument);
         }
     }
 
--- bettermemory/src/main/java/no/bettermemory/tools/DatabaseConnections.java
+++ bettermemory/src/main/java/no/bettermemory/tools/DatabaseConnections.java
@@ -12,6 +12,9 @@ public class DatabaseConnections {
     private static final String MONGODB_USERS_DATABASE = "user_info";
     private static final String MONGODB_PATIENT_COLLECTION = "patient";
     private static final String MONGODB_HEALTHCARE_PERSONNEL_COLLECTION = "healthcare_personnel";
+    private static final String MONGODB_WEEKS_COLLECTION = "weeks";
+    private static final String MONGODB_DAYS_COLLECTION = "days";
+    private static final String MONGODB_ACTIVITIES_COLLECTION = "activities";
     
     public static MongoClient getMongodbClientInfo() {
         return MongoClients.create(MONGODB_CLIENT);
@@ -27,5 +30,17 @@ public class DatabaseConnections {
 
     public static MongoCollection<Document> getHealthCarePersonnelCollection(MongoDatabase database) {
         return database.getCollection(MONGODB_HEALTHCARE_PERSONNEL_COLLECTION);
+    }
+
+    public static MongoCollection<Document> getWeeksCollection(MongoDatabase database) {
+        return database.getCollection(MONGODB_WEEKS_COLLECTION);
+    }
+
+    public static MongoCollection<Document> getDaysCollection(MongoDatabase database) {
+        return database.getCollection(MONGODB_DAYS_COLLECTION);
+    }
+
+    public static MongoCollection<Document> getActivitiesCollection(MongoDatabase database) {
+        return database.getCollection(MONGODB_ACTIVITIES_COLLECTION);
     }
 }
