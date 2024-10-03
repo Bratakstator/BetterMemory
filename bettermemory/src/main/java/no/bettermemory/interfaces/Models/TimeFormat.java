9fda97496d47766461392835543c8faa36be994f
hhamnnes <96078953+hhamnnes@users.noreply.github.com>
2024-10-03 21:10:22 +0200

    Changes was made to TimeFormat interface I hade to change the interface in order to better meet the requirement of the system

--- bettermemory/src/main/java/no/bettermemory/interfaces/Models/TimeFormat.java
+++ bettermemory/src/main/java/no/bettermemory/interfaces/Models/TimeFormat.java
@@ -6,15 +6,29 @@ package no.bettermemory.interfaces.Models;
  * This interface should be implemented when creating classes for defining 
  * a time format. 
  */
-interface TimeFormat<T, U> {
+interface TimeFormat<T> {
 
     /**
-     * This method should return a time representative format of choosing. You, the programmer
-     * will need to specify what time format you want to convert from.
-     * @param timeData
-     * @return T
+     * This method should return an datatype which represent hour.
      */
-    T convertToTimeFormat(U inputTimeFormat);
+    T getHour();
+
+    /**
+     * This method should return an datatype which represent minutes.
+     */
+    T getMinutes();
+
+
+    /**
+     * This method should return an datatype which represent weekNumber.
+     */
+    T getWeekNumber();
+
+    /**
+     * This method should return an datatype which represent year minutes.
+     */
+    T getYear();
+
 
 
     
