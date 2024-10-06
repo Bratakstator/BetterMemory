e5c41ef395492b6c6349737956515055c00ff46e
JoakimKL <joakim.for.github@gmail.com>
2024-10-06 22:18:08 +0200

    Created InsertActivityToMongoDB class

---
+++ bettermemory/src/main/java/no/bettermemory/models/storageHandlers/databaseInsertions/InsertActivityToMongoDB.java
@@ -0,0 +1,34 @@
+package no.bettermemory.models.storageHandlers.databaseInsertions;
+
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.InsertOneResult;
+
+import no.bettermemory.interfaces.storageHandlers.databaseInserters.InsertActivity;
+import no.bettermemory.models.activity.Activity;
+import no.bettermemory.tools.DatabaseConnections;
+
+public class InsertActivityToMongoDB implements InsertActivity {
+    MongoClient client;
+    MongoDatabase database;
+    MongoCollection<Document> collection;
+    
+    public InsertActivityToMongoDB(MongoClient client) {
+        this.client = client;
+        this.database = DatabaseConnections.getUsersDatabase(client);
+        this.collection = DatabaseConnections.getActivitiesCollection(database);
+    }
+
+    @Override
+    public void saveObject(Activity activity) throws Exception {
+        Document insert = activity.toDocument();
+        InsertOneResult result = collection.insertOne(insert);
+    }
+
+    @Override
+    public void updateObject(ObjectId activityId, Activity activity) throws Exception {}
+}
