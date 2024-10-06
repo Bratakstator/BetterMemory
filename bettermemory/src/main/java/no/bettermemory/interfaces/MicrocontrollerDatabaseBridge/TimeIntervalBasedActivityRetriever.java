6523f247f4fcfd41b4b700266d37afbfc4441a50
hhamnnes <96078953+hhamnnes@users.noreply.github.com>
2024-10-06 18:38:10 +0200

    I have created two neew interfaces TimeBasedActivityRetriver TimeIntervalBasedActivityRetriver

---
+++ bettermemory/src/main/java/no/bettermemory/interfaces/MicrocontrollerDatabaseBridge/TimeIntervalBasedActivityRetriever.java
@@ -0,0 +1,23 @@
+package no.bettermemory.interfaces.MicrocontrollerDatabaseBridge;
+
+/**
+ * This interface should be implemented those places where you need 
+ * a class to return some activities based on a specified time 
+ * interval.
+ */
+public interface TimeIntervalBasedActivityRetriever<T, U, I> {
+
+    /**
+     * This method should return a set of activities based on a
+     * specified timeInterval and the methods logic should use some 
+     * kind of a timeBasedActivityRetriever in order to find those
+     * activities.
+     * @param timeBasedActivityRetriever -Should be an object that implements the timeBasedActivityRetriever interface
+     * @param timeInterval 
+     * @return
+     */
+    T getActivities(U timeBasedActivityRetriever, I timeInterval);
+
+    
+    
+}
