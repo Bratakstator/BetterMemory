c8e62995f423dd8680d2186438775af2a02f3fac
JoakimKL <joakim.for.github@gmail.com>
2024-10-06 15:57:45 +0200

    Created InsertToDatabase interface - Generic interface - Has methods saveObject() and updateObject() - updateObject() is commented out until it is to be implemented

--- bettermemory/src/main/java/no/bettermemory/interfaces/storageHandlers/databaseInserters/InsertToDatabase.java
+++ bettermemory/src/main/java/no/bettermemory/interfaces/storageHandlers/databaseInserters/InsertToDatabase.java
@@ -1,6 +1,6 @@
 package no.bettermemory.interfaces.storageHandlers.databaseInserters;
 
 public interface InsertToDatabase<T> {
-    void saveObject(T t);
-    void updateObject(T t);
+    void saveObject(T t) throws Exception;
+    //void updateObject(T t) throws Exception;
 }
