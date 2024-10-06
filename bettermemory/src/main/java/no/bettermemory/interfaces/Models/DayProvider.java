e7ae932e8756c63abb9d1361612c4b352aaaa28d
hhamnnes <96078953+hhamnnes@users.noreply.github.com>
2024-10-06 17:27:38 +0200

    Created a new interface DayProvider

---
+++ bettermemory/src/main/java/no/bettermemory/interfaces/Models/DayProvider.java
@@ -0,0 +1,15 @@
+package no.bettermemory.interfaces.Models;
+
+/**
+ * This interface should be implemented in those class which needs to return a 
+ * day value;
+ */
+public interface DayProvider<T> {
+
+    /**
+    * This method should return a day representation on the specified data value format.
+    * @return T
+    */
+    T getDay();
+    
+}
