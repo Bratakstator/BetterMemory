16dab6e1c52f7a9e931fafd763f1335f93e13bd5
hhamnnes <96078953+hhamnnes@users.noreply.github.com>
2024-10-06 21:36:51 +0200

    Creation of TimeBasedActivityRetriver class

--- bettermemory/src/main/java/no/bettermemory/interfaces/MicrocontrollerDatabaseBridge/TimeBasedObjectRetriever.java
+++ bettermemory/src/main/java/no/bettermemory/interfaces/MicrocontrollerDatabaseBridge/TimeBasedObjectRetriever.java
@@ -4,22 +4,13 @@ package no.bettermemory.interfaces.MicrocontrollerDatabaseBridge;
  * This interface is meant to be used in order to return a 
  * an activity based on some set of time parameters.
  */
-public interface TimeBasedObjectRetriever<T> {
+public interface TimeBasedObjectRetriever<T, U> {
 
     /**
      * Should return the activity in a chosen data format.
-     * @param minute
-     * @param hour
-     * @param dayName
-     * @param weekNumber
-     * @param year
      * @return T
      * 
      */
-    T getObject(int minute,
-                  int hour,
-                  String dayName,
-                  int weekNumber,
-                  int year); 
+    T getObject(); 
     
 }
--- bettermemory/src/main/java/no/bettermemory/models/MicrocontrollerDatabaseBridge/TimeBasedActivityRetriever.java
+++ bettermemory/src/main/java/no/bettermemory/models/MicrocontrollerDatabaseBridge/TimeBasedActivityRetriever.java
@@ -1,5 +1,74 @@
 package no.bettermemory.models.MicrocontrollerDatabaseBridge;
 
-public class TimeBasedActivityRetriever {
-    
+import no.bettermemory.interfaces.MicrocontrollerDatabaseBridge.TimeBasedObjectRetriever;
+import no.bettermemory.interfaces.Models.DayProvider;
+import no.bettermemory.interfaces.Models.HourProvider;
+import no.bettermemory.interfaces.Models.MinutesProvider;
+import no.bettermemory.interfaces.Models.WeekProvider;
+import no.bettermemory.interfaces.Models.YearProvider;
+import no.bettermemory.interfaces.storageHandlers.storageGetters.GetActivity;
+import no.bettermemory.models.activity.Activity;
+import java.util.List;
+
+/**
+ * This class is meant to provide the activity objects which are associated with 
+ * a specific time stamp.
+ * @author Hermann Mjelde Hamnnes
+ */
+public class TimeBasedActivityRetriever implements TimeBasedObjectRetriever<List<Activity>, GetActivity> {
+
+    private MinutesProvider<Integer> minute;
+    private HourProvider<Integer> hour;
+    private DayProvider<String> dayName;
+    private WeekProvider<Integer> weekNumber;
+    private YearProvider<Integer> year;
+    private String patientId;
+    private GetActivity getActivity;
+
+    /**
+     * This method can be used to retrieve a list of Activities associated with some 
+     * time parameters provided by classes which implements some specific interfaces. 
+     * @param minute - Must be an object of a class which implements MinutesProvider
+     * @param hour - Must be an object of a class which implements HourProvider
+     * @param dayName - Must be an object of a class which implements DayProvider
+     * @param weekNumber - Must be an object of a class which implements WeekProvider
+     * @param year - Must be an object of a class which implements YearProvider
+     * @param patientId
+     * @param getActivity - Must be an object of a class which implements GetActivity
+     */
+    public TimeBasedActivityRetriever(MinutesProvider<Integer> minute,
+                                      HourProvider<Integer> hour,
+                                      DayProvider<String> dayName,
+                                      WeekProvider<Integer> weekNumber,
+                                      YearProvider<Integer> year,
+                                      String patientId,
+                                      GetActivity getActivity) {
+        
+        this.minute = minute;
+        this.hour = hour;
+        this.dayName = dayName;
+        this.weekNumber = weekNumber;
+        this.year = year;
+        this.patientId = patientId;
+        this.getActivity = getActivity;
+    }
+
+    public List<Activity> getObject() {
+
+        try{
+            return (List<Activity>) getActivity.getActivitiesAtMinute(patientId, 
+                                                                      year.getYear(),
+                                                                      weekNumber.getWeek(), 
+                                                                      dayName.getDay(), 
+                                                                      hour.getHour(), 
+                                                                      minute.getMinutes());
+        }
+
+        catch (Exception e) {
+            System.err.println(e);
+            throw new RuntimeException(e);
+        }
+
+    }
+
 }
