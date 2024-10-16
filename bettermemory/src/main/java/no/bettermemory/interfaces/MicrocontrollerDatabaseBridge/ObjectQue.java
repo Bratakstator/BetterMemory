ea11c9f50d9cf94fb6c555e7df72f157b9cb3e8d
JoakimKL <joakim.for.github@gmail.com>
2024-10-16 11:43:36 +0200

    Created ActivityQue class Also added containsElements() method to interface

--- bettermemory/src/main/java/no/bettermemory/interfaces/MicrocontrollerDatabaseBridge/ObjectQue.java
+++ bettermemory/src/main/java/no/bettermemory/interfaces/MicrocontrollerDatabaseBridge/ObjectQue.java
@@ -3,4 +3,5 @@ package no.bettermemory.interfaces.MicrocontrollerDatabaseBridge;
 public interface ObjectQue<T, U> {
     int addToQue(T listOrMapObject);
     U getFirstFromQueAsDTO();
+    boolean containsElements();
 }
