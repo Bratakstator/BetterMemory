9619e448c58ca03d9df85c734b72953ce92b71fe
H.Henrik.D <handis@hotmail.no>
2024-10-22 17:50:26 +0200

    Implemented Java code for coms to arduino

---
+++ bettermemory/src/main/java/no/bettermemory/tools/ArduinoActivityCommunicator.java
@@ -0,0 +1,11 @@
+package no.bettermemory.tools;
+import java.util.Scanner;
+import com.fazecast.jSerialComm.SerialPort;
+
+public class ArduinoActivityCommunicator {
+
+    public void ArduinoActivitySend(){
+        
+    }
+
+}
