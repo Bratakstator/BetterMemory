e6008033bdce5e1fd9c3bc96ff73e2d27dafbec2
JoakimKL <joakim.for.github@gmail.com>
2024-10-25 10:19:54 +0200

    Added parameters to ActivityQueHandler

--- bettermemory/src/main/java/no/bettermemory/models/MicrocontrollerDatabaseBridge/ActivityHandlers/ActivityQueHandler.java
+++ bettermemory/src/main/java/no/bettermemory/models/MicrocontrollerDatabaseBridge/ActivityHandlers/ActivityQueHandler.java
@@ -5,14 +5,22 @@ import java.util.Map;
 import org.bson.types.ObjectId;
 
 import no.bettermemory.interfaces.MicrocontrollerDatabaseBridge.ObjectQueHandler;
+import no.bettermemory.interfaces.MicrocontrollerDatabaseBridge.StaticContainerHandler;
 import no.bettermemory.interfaces.MicrocontrollerDatabaseBridge.TimeIntervalBasedObjectRetriever;
+import no.bettermemory.interfaces.storageHandlers.databaseInserters.InsertActivityOrDay;
 import no.bettermemory.models.DTO.ActivityDTO;
 import no.bettermemory.models.activity.Activity;
 
 public class ActivityQueHandler implements ObjectQueHandler<ActivityDTO> {
+    private InsertActivityOrDay<Activity> insertActivity;
     private TimeIntervalBasedObjectRetriever<Map<ObjectId, Activity>> activitiesMap;
+    private StaticContainerHandler<ActivityDTO> arrayHandler;
 
-    public ActivityQueHandler() {}
+    public ActivityQueHandler(
+        InsertActivityOrDay<Activity> insertActivity,
+        TimeIntervalBasedObjectRetriever<Map<ObjectId, Activity>> activitiesMap,
+        StaticContainerHandler<ActivityDTO> arrayHandler
+    ) {}
 
     public void run() {}
 
