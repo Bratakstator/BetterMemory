cba0b454fa1db4a712f0d7314053cdcc2b7b22ad
JoakimKL <88777720+Bratakstator@users.noreply.github.com>
2024-10-26 14:51:47 +0200

    Fixed warnings in ObjectQueHandler interface

--- bettermemory/src/main/java/no/bettermemory/interfaces/MicrocontrollerDatabaseBridge/ObjectQueHandler.java
+++ bettermemory/src/main/java/no/bettermemory/interfaces/MicrocontrollerDatabaseBridge/ObjectQueHandler.java
@@ -1,7 +1,5 @@
 package no.bettermemory.interfaces.MicrocontrollerDatabaseBridge;
 
-import java.util.Map;
-
 public interface ObjectQueHandler<T, U> {
     void run();
     T[] containerElementToDTOConverter(U u);
