3b4d6f2b1d5bf929a8b4712e494aa90f41565b7d
JoakimKL <88777720+Bratakstator@users.noreply.github.com>
2024-10-27 15:46:15 +0100

    Created tests for ActivityQueHandler

---
+++ bettermemory/src/test/java/no/bettermemory/models/MicrocontrollerDatabaseBridge/ActivityHandlers/QueHandlers/ActivityQueHandlerTest.java
@@ -0,0 +1,48 @@
+package no.bettermemory.models.MicrocontrollerDatabaseBridge.ActivityHandlers.QueHandlers;
+
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ActivityQueHandlerTest {
+    @Mock
+    private ActivityStateChecker mockStateChecker;
+    @Mock
+    private ActivityQueInserter mockQueInserter;
+    @InjectMocks
+    private ActivityQueHandler queHandler;
+
+    @BeforeEach
+    void setUp() {
+        queHandler = new ActivityQueHandler(mockStateChecker, mockQueInserter);
+    }
+
+    @Test
+    public void testRunMethod() throws Exception {
+        // Act
+        queHandler.run();
+
+        // Assert
+        verify(mockStateChecker).checkQueState();
+        verify(mockQueInserter).checkNullsAndAddToList();
+    }
+
+    @Test
+    public void testRunMethodWithException() throws Exception {
+        // Arrange
+        doThrow(new Exception("Exception")).when(mockQueInserter).checkNullsAndAddToList();
+
+        // Act
+        queHandler.run();
+
+        // Assert
+        verify(mockStateChecker).checkQueState();
+    }
+}
