ec9241bf6d73539b22e79a15b089404cfe6b3dd3
JoakimKL <joakim.for.github@gmail.com>
2024-10-30 12:36:40 +0100

    Added attribute String dayName to ActivityDTO

--- bettermemory/src/main/java/no/bettermemory/models/DTO/ActivityDTO.java
+++ bettermemory/src/main/java/no/bettermemory/models/DTO/ActivityDTO.java
@@ -7,17 +7,23 @@ import no.bettermemory.models.activity.Activity;
 public class ActivityDTO {
     ObjectId activityId;
     Activity activity;
+    String dayName;
 
-    public ActivityDTO(ObjectId activityId, Activity activity) {
+    public ActivityDTO(ObjectId activityId, Activity activity, String dayName) {
         this.activityId = activityId;
         this.activity = activity;
+        this.dayName = dayName;
     }
 
     public void setActivityId(ObjectId activityId) { this.activityId = activityId; }
 
     public void setActivity(Activity activity) { this.activity = activity; }
 
+    public void setDayName(String dayName) { this.dayName = dayName; }
+
     public ObjectId getActivityId() { return activityId; }
 
     public Activity getActivity() { return activity; }
+
+    public String getDayName() { return dayName; }
 }
