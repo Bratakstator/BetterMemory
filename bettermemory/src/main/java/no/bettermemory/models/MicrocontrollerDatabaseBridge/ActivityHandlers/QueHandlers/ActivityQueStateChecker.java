3b07110aaaf21d926b06cc07c834ad1a5c6f6a47
H.Henrik.D <handis@hotmail.no>
2024-11-20 16:26:39 +0100

    Documentet ActivityQueInserter and ActivityStateChecker
    and janitor work in ArduinoActivityCommunicator

--- bettermemory/src/main/java/no/bettermemory/models/MicrocontrollerDatabaseBridge/ActivityHandlers/QueHandlers/ActivityQueStateChecker.java
+++ bettermemory/src/main/java/no/bettermemory/models/MicrocontrollerDatabaseBridge/ActivityHandlers/QueHandlers/ActivityQueStateChecker.java
@@ -7,22 +7,63 @@ import no.bettermemory.models.DTO.ActivityDTO;
 import no.bettermemory.models.activity.Activity;
 import no.bettermemory.tools.TimeComparisons;
 
+
+/**
+ * This class is responsible for managing the state of an activity queue. 
+ * It periodically checks whether activities in the queue meet certain conditions 
+ * and processes them accordingly, such as marking activities as concluded or 
+ * removing outdated activities.
+ *
+ * @param arrayHandler - Handles a static container holding activity data transfer objects (DTOs).
+ * @param insertActivity - Provides functionality to update activity data in the database.
+ * @param threshold - The time threshold (in minutes) for considering an activity outdated. Default is 30 minutes.
+ * 
+ * @author Joakim Klemsdal Bøe
+ *
+ * @code
+ * To use this class:
+ * <pre>{@code
+ * StaticContainerHandler<ActivityDTO> arrayHandler = new YourArrayHandlerImplementation<>();
+ * InsertActivityOrDay<Activity> databaseInserter = new YourDatabaseInserterImplementation<>();
+ * ActivityQueStateChecker queStateChecker = new ActivityQueStateChecker(arrayHandler, databaseInserter);
+ * queStateChecker.checkQueState();
+ * }</pre>
+ */
+
 public class ActivityQueStateChecker implements ObjectQueStateChecker {
     private StaticContainerHandler<ActivityDTO> arrayHandler;
     private InsertActivityOrDay<Activity> insertActivity;
     private int threshold = 30; // Standard value
 
+
+    /**
+     * Constructor for initializing the ActivityQueStateChecker with required handlers.
+     *
+     * @param arrayHandler - A handler for managing the activity DTOs in the queue.
+     * @param insertActivity - A handler for inserting or updating activity data in the database.
+     */
+    
     public ActivityQueStateChecker(StaticContainerHandler<ActivityDTO> arrayHandler, InsertActivityOrDay<Activity> insertActivity) {
         this.arrayHandler = arrayHandler;
         this.insertActivity = insertActivity;
     }
+
+
+    /**
+     * Checks the state of the activity queue and processes activities based on their conditions.
+     * If an activity is concluded, it updates the database and removes it from the queue.
+     * If an activity exceeds the time threshold, it is removed from the queue.
+     * 
+     * @throws IndexOutOfBoundsException if there is an issue accessing the queue by index.
+     * @throws Exception for any unexpected issues during processing.
+     */
 
     @Override
     public void checkQueState() {
         for (int index = 0; index < arrayHandler.length(); index++) {
             ActivityDTO activityDTO = arrayHandler.get(index);
 
-            if (activityDTO == null) continue;
+            if (activityDTO == null) continue; // Skip if there is no activity at the current index.
 
             System.out.println("AQSC "+activityDTO.getActivity());
 
@@ -33,7 +74,7 @@ public class ActivityQueStateChecker implements ObjectQueStateChecker {
                         activityDTO.getActivity()
                     );
                     System.out.println("Activity set concluded: " + activityDTO.getActivity());
-                    arrayHandler.addAtIndex(index, null);
+                    arrayHandler.addAtIndex(index, null); // Remove the activity from the queue.
                 } catch (IndexOutOfBoundsException e) {
                     System.err.println(e);
                 } catch (Exception e) {
